ef835600c9b1277eeb18fb766f354a74cec51986
Bigard <amukamara7@gmail.com>
2023-03-29 16:41:31 +0100

    initial commit

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -11,7 +11,7 @@ function HomeScreen() {
       <Nav />
       <Banner />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow />
-      <Row title="Top rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
